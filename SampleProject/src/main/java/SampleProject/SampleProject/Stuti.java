83af48adce6c77621b025c1810d9141db66876a0
SowmyaKalliroppa <sowmya.ks21@gmail.com>
2021-06-19 12:22:26 +0530

    Hi commit

---
+++ SampleProject/src/main/java/SampleProject/SampleProject/Stuti.java
@@ -0,0 +1,10 @@
+package SampleProject.SampleProject;
+
+public class Stuti {
+
+	public static void main(String[] args) {
+		System.out.println("Hi sowmya");
+		System.out.println("Hi Stuti");
+		}
+
+}
